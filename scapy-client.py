47a4d4e44b1d50212e625fb889027051bcb9c265
Sridatta Raghavendra Chintapalli <dattu046@tamu.edu>
2024-03-14 16:32:30 -0500

    #comment modify config files and add logic to save test results

--- scapy-client.py
+++ scapy-client.py
@@ -3,29 +3,126 @@
 from scapy.all import *
 import sys
 import json
+from functools import partial
+import csv
 
 def read_config():
     with open('rules.json', 'r') as f:
         config = json.load(f)
     return config
 
-def handle_response_packet(packet):
-    if packet.haslayer(IP):
-        print("Received reply from server : ", packet.summary())
+def send_tcp(target_ip, target_port, rule):
+    print(target_ip, target_port, rule)    
+    tcp_packet = IP(dst=target_ip) / TCP(dport=target_port, flags="S")
+    send(tcp_packet)
 
-def send_packet(destination):
-    print("Sending packet to", destination)
-    packet = IP(dst=destination) / TCP(dport=80, flags="S")
-    send(packet)
+    sniff(filter=f"tcp and src host {target_ip}", prn=handle_response(partial(match_rule_to_reply, arg1=rule)), timeout=2)
 
-    sniff(filter="tcp and host " + destination, prn=handle_response_packet, timeout=1000)
+def send_udp(target_ip, target_port, rule):
+    udp_packet = IP(dst=target_ip) / UDP(dport=target_port)
+    send(udp_packet)
+
+    sniff(filter=f"udp and src host {target_ip}", prn=handle_response(partial(match_rule_to_reply, arg1=rule)), timeout=2)
 
 
+def send_icmp(target_ip, rule):
+    icmp_packet = IP(dst=target_ip) / ICMP()
+    send(icmp_packet)
+    
+    sniff(filter=f"icmp and src host {target_ip}", prn=handle_response(partial(match_rule_to_reply, arg1=rule)), timeout=2)
+
+
+def handle_response(callback, *args, **kwargs):
+    def wrapper(packet):
+        if packet.haslayer(TCP) and packet[TCP].flags & 0x12:  # Check if packet is TCP SYN-ACK
+            print("Received TCP SYN-ACK reply from", packet[IP].src)
+            # Call the callback function if a TCP reply is received
+            callback(packet, *args, **kwargs)
+        elif packet.haslayer(UDP):
+            print("Received UDP reply from", packet[IP].src)
+            # Call the callback function if a UDP reply is received
+            callback(packet, *args, **kwargs)
+        elif packet.haslayer(ICMP):
+            print("Received ICMP reply from", packet[IP].src)
+            # Call the callback function if an ICMP reply is received
+            callback(packet, *args, **kwargs)
+        else:
+            print("No expected reply received")
+            # Call another function if no expected reply is received
+            match_rule_to_no_response(*args)
+
+    return wrapper
+
+def match_rule_to_reply(packet,rule):
+    print(f"Response for rule: {rule['name']}")
+
+    packet_details = {
+        "timestamp": str(datetime.now()),
+        "source_ip": packet[IP].src,
+        "destination_ip": packet[IP].dst,
+        "protocol": packet[IP].proto,
+        "packet_length": len(packet),
+        # Add more fields as needed
+    }
+
+    if packet.haslayer(ICMP):
+        packet_details["icmp_type"] = packet[ICMP].type
+        packet_details["icmp_code"] = packet[ICMP].code
+
+    elif packet.haslayer(UDP):
+        packet_details["udp_source_port"] = packet[UDP].sport
+        packet_details["udp_destination_port"] = packet[UDP].dport
+
+    elif packet.haslayer(TCP):
+        packet_details["tcp_source_port"] = packet[TCP].sport
+        packet_details["tcp_destination_port"] = packet[TCP].dport
+        packet_details["tcp_flags"] = packet[TCP].flags
+
+    if(packet_details["protocol"].lower() == rule["ip_protocol"].lower()):
+        packet_details["Test Result"] = "Passed"
+    else:
+        packet_details["Test Result"] = "Failed"
+    
+    write_to_csv(packet_details)
+
+    
+def match_rule_to_no_response(rule):
+    print(f"No response for rule: {rule['name']}")
+
+    packet_details = {
+        "timestamp": str(datetime.now())
+    }
+    if(packet_details["protocol"].lower() == rule["ip_protocol"].lower()):
+        packet_details["Test Result"] = "Failed"
+    else:
+        packet_details["Test Result"] = "Passed"
+    
+    write_to_csv(packet_details)
+
+def write_to_csv(packet_details):
+    with open("test_results.csv", "a", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=packet_details.keys())
+        if csv_file.tell() == 0:  # Check if file is empty, write headers if needed
+            writer.writeheader()
+        writer.writerow(packet_details)
+
+def send_packet(config, destination):
+    print(f"Sending packet to {destination}")
+    print(config['firewall_rules'])
+    for rule in config['firewall_rules']:
+        print(f'Checking rule: {rule["name"]}')
+        print(f"Allowed Protocol: {rule['ip_protocol']}")
+        print(f"Allowed Port Range: {rule['from_port']}-{rule['to_port']}")
+        for port in range(rule['from_port'], rule['to_port'] + 1):
+            send_tcp(destination, port, rule)
+            #send_udp(destination, port, rule)
+            #send_icmp(destination, rule)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python scapy-client.py <destination>")
         sys.exit(1)
     destination = sys.argv[1]
-    conf = read_config()
-    send_packet(destination)
+    config = read_config()
+    print(config)
+    send_packet(config,destination)
--- scapy-server.py
+++ scapy-server.py
@@ -1,20 +1,98 @@
 # The server should listen on all possible ports and log any data received from any client and send it back.
 from scapy.all import *
 import sys
+import json
+import csv
+from datetime import datetime
+
+def read_port_info():
+    with open('server_ports.json', 'r') as f:
+        config = json.load(f)
+    return config
+
+def get_ip_address():
+    try:
+        ip_address = socket.gethostbyname(socket.gethostname())
+        return ip_address
+    except socket.error as e:
+        print("Error occurred while getting IP address:", e)
+        return None
+    
 
 def handle_packet(packet):
-    print("Responding to client with:", packet.summary())
-    send(packet)
+    packet_details = {
+        "timestamp": str(datetime.now()),
+        "source_ip": packet[IP].src,
+        "destination_ip": packet[IP].dst,
+        "protocol": packet[IP].proto,
+        "packet_length": len(packet)
+    }
 
-def start_server(ports):
-    print("Starting server")
+    if packet.haslayer(TCP):
+        handle_tcp(packet)
+        packet_details["tcp_source_port"] = packet[TCP].sport
+        packet_details["tcp_destination_port"] = packet[TCP].dport
+        packet_details["tcp_flags"] = packet[TCP].flags
+    elif packet.haslayer(UDP):
+        handle_udp(packet)
+        packet_details["udp_source_port"] = packet[UDP].sport
+        packet_details["udp_destination_port"] = packet[UDP].dport
+    elif packet.haslayer(ICMP):
+        handle_icmp(packet)
+        packet_details["icmp_type"] = packet[ICMP].type
+        packet_details["icmp_code"] = packet[ICMP].code
+
+    with open("packet_details.csv", "a", newline="") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=packet_details.keys())
+        if csv_file.tell() == 0:  # Check if file is empty, write headers if needed
+            writer.writeheader()
+        writer.writerow(packet_details)
+
+
+def handle_tcp(packet):
+    if packet[TCP].flags & 0x02:  # Check if packet is SYN
+        print("Received TCP SYN packet from", packet[IP].src)
+
+        # Craft TCP SYN-ACK reply
+        reply = IP(dst=packet[IP].src) / TCP(dport=packet[TCP].sport, sport=packet[TCP].dport, flags="SA")
+        send(reply)
+
+        print("Sent TCP SYN-ACK reply to", packet[IP].src)
+
+
+def handle_udp(packet):
+    print("Received UDP packet from", packet[IP].src)
+
+    # Craft UDP reply
+    reply = IP(dst=packet[IP].src) / UDP(dport=packet[UDP].sport, sport=packet[UDP].dport)
+    send(reply)
+
+    print("Sent UDP reply to", packet[IP].src)
+
+
+def handle_icmp(packet):
+    if packet[ICMP].type == 8:  # Check if packet is ICMP echo request
+        print("Received ICMP Echo Request from", packet[IP].src)
+
+        # Craft ICMP Echo Reply
+        reply = IP(dst=packet[IP].src) / ICMP(type=0) / packet[Raw].load
+        send(reply)
+
+        print("Sent ICMP Echo Reply to", packet[IP].src)
+
+
+def start_server(ports, src_ip, host):
+    print("Started server on", host, "on ports", ports)
     for port in ports:
-        sniff(filter=f"port {port} and host 127.0.0.1", prn=handle_packet, iface=interface_name, store=0)
+        sniff(filter=f"(tcp or udp or icmp) and not src host {host} and port {port} and host {src_ip}", prn=handle_packet, iface=interface_name, store=0)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python scapy-server.py <interface_name>")
+    if len(sys.argv) < 3:
+        print("Usage: python scapy-server.py <interface_name> <src_ip>")
         sys.exit(1)
     interface_name = sys.argv[1]
-    ports = [80]
-    start_server(ports)
+    src_ip = sys.argv[2]
+    host = get_ip_address()
+    print(f"Server IP: {host}")
+    ports = read_port_info()["ports"]
+    start_server(ports, src_ip, host)
